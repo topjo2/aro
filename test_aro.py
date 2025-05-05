499d2575135f8a67ac3860ebb7f339890c89c4d7
Votre Nom <votre@email.com>
2025-05-05 13:08:25 +0200

    Version initiale d'Aro

--- test_aro.py
+++ test_aro.py
@@ -1,11 +1,12 @@
+# test_aro.py
 import aro
-import aro
-print(aro.salutation())
-assert aro.multiplier(3, 4) == 12  # Test silencieux si réussi
+import unittest
 
-def test_aro():
-    print(aro.salutation())
-    assert aro.multiplier(3, 4) == 12
+class TestAro(unittest.TestCase):
+    def test_fonctions(self):
+        self.assertEqual(aro.salutation(), "Bonjour, je suis Aro, votre assistante Rust! 🟡")
+        self.assertEqual(aro.multiplier(3, 4), 12)
+        self.assertEqual(aro.au_revoir(), "Au revoir, merci d'avoir utilisé Aro! 🟢")
 
 if __name__ == "__main__":
-    test_aro()
+    unittest.main()
